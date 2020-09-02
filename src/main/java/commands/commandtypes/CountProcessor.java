c2cb138afaad21c86b212c72ed6e73dfd7438905
Ayapha98 <ayaphamihlali@gmail.com>
2020-09-02 10:11:04 +0200

    added the Count command

---
+++ src/main/java/commands/commandtypes/CountProcessor.java
@@ -0,0 +1,11 @@
+package commands.commandtypes;
+
+import commands.commandprocesses.Command;
+import commands.commandprocesses.Process;
+
+public class CountProcessor implements Process {
+    @Override
+    public String process(Command command) {
+        return null;
+    }
+}
